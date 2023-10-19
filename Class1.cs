b379b7d4145e02e006e234333e4342a8b76d34f5
Olexandr <124339852+OLeksandr344@users.noreply.github.com>
2023-10-19 23:10:49 +0300

    1

---
+++ Class1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{ 
+    internal class Car
+    {
+        public string Name;
+        public string Color;
+        public double Price;
+        public const string CompanyName = "MyCarCompany";
+
+        public Car()
+        {
+            Name = "Default_Car";
+            Color = "White";
+            Price = 10000;
+        }
+        public Car(string name)
+        {
+            Name = name;
+            Color = "White";
+            Price = 10000;
+        }
+        public Car(string name, string color)
+        {
+            Name = name;         
+            Color = color;
+            Price = 10000;
+        }
+
+        public Car(string name, string color, double price)
+        {
+            Name = name;
+            Color = color;
+            Price = price;
+        }
+        public void InputName(string name1)
+        {
+            Name = name1;
+        }
+        public void InputColor(string color1) 
+        {          
+            Color = color1;
+        }
+        public void InputPrice(double price1)
+        {
+            Price = price1;
+        }
+        public void PrintName() {Console.WriteLine($"Car Name: {Name}"); }
+        public void PrintColor() { Console.WriteLine($"Car Color: {Color}"); }
+        public void PrintPrice() { Console.WriteLine($"Car Price: {Price}"); }
+        public void PrintCompany() { Console.WriteLine($"Car Price: {CompanyName}"); }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Car Name: {Name}");
+            Console.WriteLine($"Car Color: {Color}");
+            Console.WriteLine($"Car Price: {Price}");
+            Console.WriteLine($"Company Name: {CompanyName}");
+        }
+        public void ChangePrice(double x)
+        {          
+            double percent_change = x / 100;
+            Price = Price * (1 + percent_change);                              
+            Console.WriteLine($"Ціна автомобіля була змінена на {x}%.");
+        }
+    }
+
+    internal class Person
+    {
+        private string Name;
+        private DateTime BirthYear;
+        
+        public string GetName
+        {
+            get { return Name; }
+        }
+        public DateTime GetBirthYear
+        {
+            get { return BirthYear; }
+        }
+        public string SetName
+        {           
+            set { Name = value; }
+        }
+        public Person()
+        {
+            Name = "NoName";
+            BirthYear = new DateTime(2000, 1, 1);
+        }
+        public Person(string name)
+        {           
+            Name = name;
+            BirthYear = new DateTime(2000, 1, 1);
+        }
+        public Person(DateTime birth_date)
+        {
+            Name = "NoName";
+            BirthYear = birth_date;
+        }
+        public Person(string name, DateTime birth_date)
+        {
+            Name = name;
+            BirthYear = birth_date;
+        }
+
+        public int Age() 
+        {
+            DateTime todayDate = DateTime.Today;
+            int age = todayDate.Year - BirthYear.Year;
+            return age;
+        }
+        public void ChangeName(string name)
+        {
+            Name = name;
+        }
+        public string ToTheString()
+        {
+            string info = $"Іи'я - {Name}, дата народження - {BirthYear}, вік {Age()} ";
+            return info;
+        }
+        public void Output()
+        {
+            Console.WriteLine(ToTheString());
+        }
+        
+    }
+    
+}
+
---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿using ConsoleApp5;
+using System;
+using System.Collections.Generic;
+
+internal class Program
+
+    {
+        static void Main(string[] args) 
+    {
+        Car myCar1 = new Car();
+        Car myCar2 = new Car("BMW");
+        Car myCar3 = new Car("AUDI","White", 20000);
+        myCar1.ChangePrice(-10);
+        myCar2.ChangePrice(-10);
+        myCar3.ChangePrice(-10);
+
+        myCar3.InputColor("Red");
+        myCar3.PrintColor();
+        myCar3.PrintInfo();
+        Console.WriteLine("------------------");
+        Person myPerson1 = new Person();
+        Person myPerson2 = new Person("Olexandr", new DateTime(2004, 11, 18));
+        Person myPerson3 = new Person("Taras", new DateTime(1976, 1, 1));
+        Person myPerson4 = new Person("Anton", new DateTime(2014, 12, 19));
+        Person myPerson5 = new Person("Mykhaylo", new DateTime(2012, 5, 30));
+        Person myPerson6 = new Person("Sergiy", new DateTime(2002, 5, 18));      
+        
+        Console.WriteLine("------------------");
+        Console.WriteLine($"{myPerson1.GetName} {myPerson1.Age()}");
+        Console.WriteLine($"{myPerson2.GetName} {myPerson2.Age()}");
+        Console.WriteLine($"{myPerson3.GetName} {myPerson3.Age()}");
+        Console.WriteLine($"{myPerson4.GetName} {myPerson4.Age()}");
+        Console.WriteLine($"{myPerson5.GetName} {myPerson5.Age()}");
+        Console.WriteLine($"{myPerson6.GetName} {myPerson6.Age()}");
+        List<Person> people = new List<Person> {myPerson1, myPerson2, myPerson3, myPerson4, myPerson5, myPerson6};      
+        foreach (Person person in people)
+        {
+            if (person.Age() < 16)
+            {
+                person.SetName = "Very Young";
+            }
+        }
+        myPerson1.Output();
+        myPerson2.Output();
+        myPerson3.Output();
+        myPerson4.Output();
+        myPerson5.Output();
+        myPerson6.Output();
+        Console.WriteLine("------------------");
+        Dictionary<string, int> nameCount = new Dictionary<string, int>();
+
+        foreach (Person person in people)
+        {
+            string name = person.GetName;
+          
+            if (nameCount.ContainsKey(name))
+            {
+                nameCount[name]++;
+            }
+            else
+            {
+                nameCount[name] = 1;
+            }                    
+        }       
+        foreach (Person person in people)
+        {
+            foreach (var i in nameCount)
+            {
+                string name_key = i.Key;
+                int count = i.Value;
+                
+                if (count > 1)
+                {
+                    if(name_key == person.GetName)
+                    {
+                        person.Output();
+                    }
+                }
+            }
+        }
+           
+
+
+
+
+
+
+
+
+
+    }
+}
+    
